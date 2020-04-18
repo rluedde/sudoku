757b1138be77b1691a6fddcbe5dbbf3f365f1d45
Reilley Luedde <rluedde@gmail.com>
2020-04-18 09:19:45 -0700

    Create Sudoku class
    Add print_board() to Sudoku class, add simple instantiation mechanism
    for Sudoku class

--- sudoku.py
+++ sudoku.py
@@ -7,41 +7,42 @@
 #   -Incorporate a database?
 #       -What games the algo can solve the fastest?
 #       -If I do a UI, store the order of the user's moves?
-
-grid = [["0", "0", "0", "0", "0", "0", "0", "0", "0"], 
-           ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
-           ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
-           ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
-           ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
-           ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
-           ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
-           ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
-           ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
-           ["0", "0", "0", "0", "0", "0", "0", "0", "0"]
-        ]
+#   -Use OOP to represent the grid?
 
 
-
-def main():
-    pass
+class Sudoku:
 
 
+    def __init__(self):
+        self.grid = [["0", "0", "0", "0", "0", "0", "0", "0", "0"], 
+                     ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                     ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                     ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                     ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                     ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                     ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                     ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                     ["0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                     ["0", "0", "0", "0", "0", "0", "0", "0", "0"]]
 
 
-def print_grid(grid):
-    row_sep = "-------------"
-    for i in range(9):
-        line = grid[i] 
-        elements_passed = 0
-        if i % 3 == 0:
-            print(row_sep)
-        for z in range(13): 
-            if elements_passed == 3 or z == 0:
-                line.insert(z, "|")
-                elements_passed = 0
-            else:
-                elements_passed += 1
-        print("".join(line))
-    print(row_sep)
+    def print_grid(self):
+        row_sep = "-------------"
+        for i in range(9):
+            line = self.grid[i] 
+            elements_passed = 0
+            if i % 3 == 0:
+                print(row_sep)
+            for z in range(13): 
+                if elements_passed == 3 or z == 0:
+                    line.insert(z, "|")
+                    elements_passed = 0
+                else:
+                    elements_passed += 1
+            print("".join(line))
+        print(row_sep)
 
-print_grid(grid)
+
+
+game = Sudoku()
+game.print_grid()
