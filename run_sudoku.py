418fba2dc01edbcb2f5c0f36a7d3c050f25ca564
Reilley Luedde <rluedde@gmail.com>
2020-04-30 07:13:11 -0700

    Don't print grid after a guess formatted badly
    There's a bug in these hills, the board prints if user gives a guess
    that's formatted well but one of the answer elements are out of range. If
    they do 3,4,17 the board will still print even though there's been no
    guess.

--- run_sudoku.py
+++ run_sudoku.py
@@ -5,25 +5,38 @@ class InputError(BaseException):
 
 def main():
     g = Sudoku()    
-    print("\nWelcome to Sudoku!")
-    print("Guess \"q\" to quit!")
-    print("An answer is structured like this: row, col, answer")
-    print("Ex. 8,8,9 - no spaces!\n")
+    print_instructions()
+    first_guess = True
     while g.game_over() == False:
-    # for i in range():
-        print("Here's the grid:\n")
-        g.print_grid()
+        # Show user the grid if it's their first guess
+        if first_guess:
+            print("Here's the grid:\n")
+            g.print_grid()
+            first_guess = False
         try: 
             play = input("\nGive your answer (r,c,a): ")
             if play.strip() == "q":
                 return "Have a good day!"
             row, col, answer = (int(num) for num in play.split(","))
+            # If the user enters a guess in the right format
+            # but the bounds on one of the guess elements are wrong, 
+            # the board gets printed even though there's no change.
+            # Only print the board when there's a change....
             g.make_answer(row,col,answer)
+            g.print_grid()
         except ValueError:
             print("Please enter the answer in the right format!")
         except InputError:
             print("Row, col, and answer must be 1-9")
     return "Victory!"
         
+
+def print_instructions():
+    print("\nWelcome to Sudoku!")
+    print("Guess \"q\" to quit!")
+    print("An answer is structured like this: row, col, answer")
+    print("Ex. 8,8,9 - no spaces!\n")
+
+
 if __name__ == "__main__":
     print(main())
