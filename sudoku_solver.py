5044613d1c06e8945254b1f47337bb47bb1df570
reilley <reilleyluedde@nau.edu>
2020-05-20 06:56:53 -0700

    This solver generates a board
    If you use this version of the solver on a board of all zeros, it
    quickly give you a valid board. There's no randomness involved so it's
    always the same board, but it's still nice

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -2,27 +2,15 @@ import numpy as np
 from sudoku import Sudoku
 from time import sleep
 
-missing_3 = np.array([[0,3,2,5,6,0,0,4,8],
-                      [5,4,6,0,8,0,2,0,7],
-                      [9,7,0,2,0,0,0,3,5],
-                      [2,0,4,9,1,0,7,0,0],
-                      [0,0,0,6,3,2,0,9,4],
-                      [3,8,9,0,0,5,0,2,6],
-                      [8,5,7,0,2,3,0,6,9],
-                      [6,9,1,0,5,0,0,8,2],
-                      [4,2,3,0,0,0,5,7,0]])
-
-hard_one =  np.array([[0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,3,0,8,5],
-                      [0,0,1,0,2,0,0,0,0],
-                      [0,0,0,5,0,7,0,0,0],
-                      [0,0,4,0,0,0,1,0,0],
-                      [0,9,0,0,0,0,0,0,0],
-                      [5,0,0,0,0,0,0,7,3],
-                      [0,0,2,0,1,0,0,0,0],
-                      [0,0,0,0,4,0,0,0,9]])
-
-
+sudoku =    np.array([[8, 1, 0, 0, 3, 0, 0, 2, 7], 
+                      [0, 6, 2, 0, 5, 0, 0, 9, 0], 
+                      [0, 7, 0, 0, 0, 0, 0, 0, 0], 
+                      [0, 9, 0, 6, 0, 0, 1, 0, 0], 
+                      [1, 0, 0, 0, 2, 0, 0, 0, 4], 
+                      [0, 0, 8, 0, 0, 5, 0, 7, 0], 
+                      [0, 0, 0, 0, 0, 0, 0, 8, 0], 
+                      [0, 2, 0, 0, 1, 0, 7, 5, 0], 
+                      [3, 8, 0, 0, 7, 0, 0, 4, 2]])
 
 
 
@@ -32,115 +20,42 @@ class SudokuSolver(Sudoku):
 
     def __init__(self, grid):
         self.grid = grid
-        self.spots_solved = 0
-        self.tried_elements = [[] for i in range(81)]
-        self.given_elements = self.grid != 0
+        self.spots_solved = 0 
 
-
-    def solve(self):
-        # Get rid of all future guesses and all future tried elements lists
-        self.wipe_future(self.spots_solved)
-        print("\n","solved:", self.spots_solved, )
+        
+    # Improvements:
+    # -Go through and find all spots where the len(candidates) == 1
+    #   fill in that 
+    # cell and then go through and run the recursive backtracker?
+    # -Keep track of trees more efficiently? 
+    # -Xwing
+    def solve(self, spots_solved = 0):
         self.print_grid()
-        row_index = self.get_row_index(self.spots_solved)
-        col_index = self.get_col_index(self.spots_solved)
 
-        print("tries:", self.tried_elements)
+        # This might work if you eliminate possibilities somehow
+        if spots_solved == 81:
+            return True
 
-        sleep(.25)
+        row_index = self.get_row_index(spots_solved)
+        col_index = self.get_col_index(spots_solved)
 
-        # Exit condition
-        if self.spots_solved == 81:
-            return self.grid
-        # Improvements:
-        # -Go through and find all spots where the len(candidates) == 1
-        #   fill in that 
-        # cell and then go through and run the recursive backtracker?
-        # -Keep track of trees more efficiently? 
-        # -Xwing
-        else: # find a solution or backtrack
-            candidates = self.get_cell_candidates(row_index,
-                                                  col_index,
-                                                  self.spots_solved)
-            print("candies:", candidates)
-            # if spot's given, move on
-            if self.given_elements[row_index,col_index]:
-                self.spots_solved += 1
-                self.solve()
+        if self.grid[row_index, col_index] == 0:
 
-            # if the cell isn't given and has no solutions, backtrack 
-            elif not self.given_elements[row_index, col_index] and\
-            len(candidates) == 0:
-                # go back to closest cell that's not given
-                while True:
-                    self.reset_curr_cell(self.spots_solved)
-                    if self.spots_solved == 0:
-                        break
-                    self.spots_solved -= 1
-                    row_index = self.get_row_index(self.spots_solved)
-                    col_index = self.get_col_index(self.spots_solved)
-                    self.wipe_future(self.spots_solved)
-                    if not self.given_elements[row_index, col_index]:
-                        break
-                # self.grid[row_index,col_index] = 0
-                self.solve()
-            
-            # if there's solutions, pick one randomly, record that we've
-            # tried it
-            else:
-                new_element = int(np.random.choice(candidates,1))
-                self.grid[row_index,col_index] = new_element
-                self.spots_solved += 1
-                self.tried_elements[self.spots_solved - 1].append(new_element)
-                self.solve()
+            candidates = self.get_cell_candidates(row_index, col_index)
 
+            for candidate in candidates:
+                self.grid[row_index, col_index] = candidate
 
+                if self.solve(spots_solved + 1):
+                    return True
 
-    """
-        # If a cell doesn't have a solution in it, try to get one.
-        elif not self.given_elements[row_index, col_index]:
-            candidates = self.get_cell_candidates(row_index,
-                                                  col_index)
-            sleep(.5)
+                self.grid[row_index, col_index] = 0
 
-            print("candies: ", candidates)
-            while len(candidates) == 0:
-                # Get back to a non-given position where there are
-                # possibilities
-                # TODO: potentially set element to 0 in here
-
-                row_index = self.get_row_index(self.spots_solved)
-                col_index = self.get_col_index(self.spots_solved)
-                candidates = self.get_cell_candidates(row_index,
-                                                      col_index)
-
-                # if it's a given cell (True): go back a cell
-                # elif it's a nongiven cell (False): try a guess that hasn't
-                # been tried yet
-                if self.given_elements[row_index, col_index] or\
-                len(candidates == 0):
-                    print("carti!")
-                    self.spots_solved -= 1
-                elif not self.given_elements[row_index,col_index] and\
-                len(candidates) != 0:
-                    self.solve()
-                # else: just keep decrementing
-                    
-
-            # Aka, if there are possibilities for the cell:
-            else:
-                new_element = int(np.random.choice(candidates,1))
-                self.grid[row_index,col_index] = new_element
-                self.spots_solved += 1
-                self.tried_elements[self.spots_solved - 1].append(new_element)
-                self.solve()
-
-
-        # If a cell is already solved, move on
         else:
-            self.spots_solved += 1
-            self.solve()
-    """
+            self.solve(spots_solved + 1)
+
+        return False
+
 
     def get_row_index(self, spots_solved):
         return spots_solved // 9 
@@ -150,31 +65,8 @@ class SudokuSolver(Sudoku):
         return spots_solved % 9
 
 
-    # Once I've gone down a branch that doesn't pan out, I want to remove all 
-    # future information that I've tried (future invalids and future guesses
-    # It might have to be the spot AFTER the current one, we don't want
-    # to remove the current cell's trieds but we do want to remove it's
-    # actual guess
-    # TODO: make sure the above shit happens
-    def wipe_future(self, spots_solved):
-        for i in range(self.spots_solved + 1, 81):
-            row_index = self.get_row_index(i)
-            col_index = self.get_col_index(i)
-            self.tried_elements[i] = []
-        
-        
-    def reset_curr_cell(self, spots_solved):
-        row_index = self.get_row_index(spots_solved)
-        col_index = self.get_col_index(spots_solved)
-        if not self.given_elements[row_index, col_index]:
-            self.grid[row_index, col_index] = 0
-
-         
-
-
     # Get all of the possible values that a cell can take
-    # TODO: if we've tried a value, it's not a candidate
-    def get_cell_candidates(self, row_index, col_index, spots_solved):
+    def get_cell_candidates(self, row_index, col_index):
         candidates = np.arange(1,10)
         if self.grid[row_index,col_index] != 0:
             return []
@@ -183,21 +75,14 @@ class SudokuSolver(Sudoku):
                 # Array version of list.remove():
                 candidates = np.delete(candidates, 
                                        np.where(candidates == candidate))
-        # remove all candidates that we've already tried:
-        tries = set(self.tried_elements[self.spots_solved])
-        candidates = set(candidates)
-
-        print("infunc candies:", candidates)
-        print("infunc tries:", tries)
-
-        candidates -= tries
         
-        return list(candidates)
+        return candidates
 
 
 
-solv = SudokuSolver(hard_one)
-solv.solve()
+
+solv = SudokuSolver(sudoku)
+print(solv.solve())
 
 
 
